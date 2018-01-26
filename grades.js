cc74c8b375b8b78909506cc9b58da13d72c30584
Dan Watson <danwatson00@yahoo.com>
2018-01-26 15:40:15 -0600

    finished

--- grades.js
+++ grades.js
@@ -1,6 +1,12 @@
 const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
 const grades = {} // You'll need to change this line of code
 grades.A = 0;
+grades.B = 0;
+grades.C = 0;
+grades.D = 0;
+grades.F = 0;
+topScore = 0;
+lowScore = 0;
 
 for (let i = 0; i < scores.length; i++) {
     if (scores[i] >= 90) {
@@ -27,3 +33,26 @@ console.log("B:" + grades.B);
 console.log("C:" + grades.C);
 console.log("D:" + grades.D);
 console.log("F:" + grades.F);
+
+console.log(Math.max(...scores));
+console.log(Math.min(...scores));
+
+var commonScores = [grades.A, grades.B, grades.C, grades.D, grades.F];
+commonScores.sort(function (a, b) {return b - a});
+console.log(commonScores[0]);
+
+var uncommonScores = [grades.A, grades.B, grades.C, grades.D, grades.F];
+uncommonScores.sort(function (a,b) {return a - b});
+console.log(uncommonScores[0]);
+
+
+function myFunction() {
+    grades.sort(function(a, b){return a.grades - b.grades});
+    highScores();
+    
+}
+
+
+
+
+
